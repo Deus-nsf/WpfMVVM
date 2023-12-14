a112abf41ced06df47aeed35410cec7575414086
Mehdi MIEG <mmieg@diginamic-formation.fr>
2023-12-14 18:13:21 +0100

    Add project files.

---
+++ WpfMVVM.DataBinding/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+using CommunityToolkit.Mvvm.Input;
+namespace WpfMVVM.DataBinding.ViewModels;
+
+
+internal class LesControlesViewModel : INotifyPropertyChanged
+{
+	public string Test { get; set; }
+	public ObservableCollection<string> Villes { get; set; } = new();
+
+
+	public event PropertyChangedEventHandler? PropertyChanged;
+	protected void OnPropertyChanged(string name)
+	{
+		PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+	}
+
+
+	public LesControlesViewModel()
+	{
+		Test = "Hello";
+	}
+
+
+	public ICommand EditTest
+	{
+		get
+		{
+			return new RelayCommand(() =>
+			{
+				Test = "Pouet";
+				OnPropertyChanged(nameof(Test));
+			});
+		}
+	}
+
+}
